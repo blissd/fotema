fc5c1701bd6441646f68678cbc9d009252c6f8a1
David Bliss <vivid.map6767@davidbliss.me>
2024-03-11 21:16:16 +0000

    Scanner

--- src/scanner.rs
+++ src/scanner.rs
@@ -1,6 +1,7 @@
 use std::path::Path;
 use std::path::PathBuf;
 use std::{env, fs};
+use walkdir::{DirEntry, WalkDir};
 
 pub struct Scanner {
     scan_path: PathBuf,
@@ -11,5 +12,26 @@ impl Scanner {
         fs::create_dir_all(scan_path).map_err(|e| e.to_string())?;
         let scan_path = PathBuf::from(scan_path);
         Ok(Scanner { scan_path })
+    }
+
+    pub fn scan(&self) {
+        WalkDir::new(&self.scan_path)
+            .into_iter()
+            .for_each(|x| println!("{}", x.unwrap().path().display()));
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn scanner_build() {
+        let mut test_data_dir = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+        test_data_dir.push("resources/test");
+        println!("{}", test_data_dir.display());
+
+        let s = Scanner::build(&test_data_dir).unwrap();
+        s.scan();
     }
 }
