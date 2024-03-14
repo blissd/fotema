93a719b97b598c1b89e8e77b09742b67af3830f6
David Bliss <vivid.map6767@davidbliss.me>
2024-03-14 12:46:57 +0000

    Move repo setup to method

--- src/repo.rs
+++ src/repo.rs
@@ -9,7 +9,11 @@ pub struct PicturesRepo {
 impl PicturesRepo {
     pub fn build(_dir_ignored_while_in_memory: &path::Path) -> Result<PicturesRepo, String> {
         let con = Connection::open_in_memory().map_err(|e| e.to_string())?;
+        let repo = PicturesRepo { con };
+        repo.setup().map(|_| repo)
+    }
 
+    fn setup(&self) -> Result<(), String> {
         let sql = "CREATE TABLE IF NOT EXISTS PICTURES (
                         path           TEXT PRIMARY KEY UNIQUE ON CONFLICT REPLACE,
                         fs_modified_at TEXT,
@@ -18,10 +22,8 @@ impl PicturesRepo {
                         description    TEXT
                         )";
 
-        let result = con.execute(&sql, ());
-        result
-            .map(|_| PicturesRepo { con })
-            .map_err(|e| e.to_string())
+        let result = self.con.execute(&sql, ());
+        result.map(|_| ()).map_err(|e| e.to_string())
     }
 
     pub fn add(&self, pic: &PictureInfo) -> Result<(), String> {
