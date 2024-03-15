46e16d8cd1c2a74399f0270ab69cbc6b13f2e1a0
David Bliss <vivid.map6767@davidbliss.me>
2024-03-15 14:02:46 +0000

    Add Repository.all()

--- src/repo.rs
+++ src/repo.rs
@@ -2,6 +2,7 @@ use crate::model::PictureInfo;
 use crate::Error::*;
 use crate::Result;
 use rusqlite::Connection;
+use rusqlite::{MappedRows, Row};
 use std::path;
 
 pub struct Repository {
@@ -9,7 +10,7 @@ pub struct Repository {
 }
 
 impl Repository {
-    pub fn build(_dir_ignored_while_in_memory: &path::Path) -> Result<Repository > {
+    pub fn build(_dir_ignored_while_in_memory: &path::Path) -> Result<Repository> {
         let con = Connection::open_in_memory().map_err(|e| RepositoryError(e.to_string()))?;
         let repo = Repository { con };
         repo.setup().map(|_| repo)
@@ -26,11 +27,12 @@ impl Repository {
                         )";
 
         let result = self.con.execute(&sql, ());
-        result.map(|_| ()).map_err(|e| RepositoryError(e.to_string()))
+        result
+            .map(|_| ())
+            .map_err(|e| RepositoryError(e.to_string()))
     }
 
     pub fn add(&self, pic: &PictureInfo) -> Result<()> {
-
         // Pictures are orderd by this UTC date time.
         let order_by_ts = pic.modified_at.map(|d| d.to_utc()).or(pic.fs_modified_at);
 
@@ -53,6 +55,49 @@ impl Repository {
             Err(e) => Err(RepositoryError(e.to_string())),
         }
     }
+
+    pub fn all(&self) -> Result<Vec<PictureInfo>> {
+        let mut stmt = self
+            .con
+            .prepare(
+                "SELECT 
+            path,
+            fs_modified_at,
+            modified_at, 
+            created_at, 
+            description
+            from PICTURES order by order_by_ts ASC",
+            )
+            .unwrap();
+
+        fn row_to_picture_info(row: &Row<'_>) -> rusqlite::Result<PictureInfo> {
+            let path_result: rusqlite::Result<String> = row.get(0);
+            if let Some(path) = path_result.ok() {
+                Ok(PictureInfo {
+                    path: path::PathBuf::from(path),
+                    // order_by_ts: row.get(0).ok(),
+                    fs_modified_at: row.get(1).ok(),
+                    modified_at: row.get(2).ok(),
+                    created_at: row.get(3).ok(),
+                    description: row.get(4).ok(),
+                })
+            } else {
+                Err(rusqlite::Error::ExecuteReturnedResults) // probably not the right error
+            }
+        }
+        let iter = match stmt.query_map([], |row| row_to_picture_info(row)) {
+            Ok(f) => f,
+            Err(e) => return Err(RepositoryError(e.to_string())),
+        };
+
+        // Would like to return an iterator... but Rust is defeating me.
+        let mut pics = Vec::new();
+        for pic in iter.flatten() {
+            pics.push(pic);
+        }
+
+        Ok(pics)
+    }
 }
 
 #[cfg(test)]
@@ -74,7 +119,11 @@ mod tests {
         let mut test_file = test_data_dir.clone();
         test_file.push("Birdie.jpg");
 
-        let pic = PictureInfo::new(test_file);
+        let pic = PictureInfo::new(test_file.clone());
         r.add(&pic).unwrap();
+
+        let all_pics = r.all().unwrap();
+        let pic = all_pics.get(0).unwrap();
+        assert_eq!(pic.path, test_file);
     }
 }
