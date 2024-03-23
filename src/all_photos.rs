9e5a3a1c116e6a2d8cabadb85f8cb495e30321af
David Bliss <vivid.map6767@davidbliss.me>
2024-03-23 13:14:00 +0000

    disable unbind

--- src/all_photos.rs
+++ src/all_photos.rs
@@ -3,7 +3,10 @@ use gtk::prelude::{BoxExt, OrientableExt};
 use photos_core;
 use relm4::factory::{DynamicIndex, FactoryComponent, FactorySender, FactoryVecDeque};
 use relm4::gtk;
+use relm4::gtk::prelude::Cast;
+use relm4::gtk::prelude::ListItemExt;
 use relm4::gtk::prelude::WidgetExt;
+use relm4::typed_view::grid::{RelmGridItem, TypedGridView};
 use relm4::*;
 use std::path;
 
@@ -17,39 +20,47 @@ pub struct PicturePreview {
     path: path::PathBuf,
 }
 
-#[relm4::factory(pub)]
-impl FactoryComponent for PicturePreview {
-    type Init = path::PathBuf;
-    type Input = InputMsg;
-    type Output = ();
-    type CommandOutput = ();
-    type ParentWidget = gtk::FlowBox;
+pub struct Widgets {
+    picture: gtk::Picture,
+}
 
-    view! {
-    #[root]
-        gtk::Box {
-            set_orientation: gtk::Orientation::Vertical,
-            set_margin_all: 1,
+impl RelmGridItem for PicturePreview {
+    type Root = gtk::Box;
+    type Widgets = Widgets;
 
-            gtk::Picture {
-                set_filename: Some(&self.path),
-                set_can_shrink: true,
-                set_width_request: 200,
-                set_height_request: 200,
+    fn setup(_item: &gtk::ListItem) -> (gtk::Box, Widgets) {
+        relm4::view! {
+           my_box = gtk::Box {
+                set_orientation: gtk::Orientation::Vertical,
+                set_margin_all: 1,
+
+                #[name = "picture"]
+                gtk::Picture {
+                    set_can_shrink: true,
+                    set_valign: gtk::Align::Center,
+                    set_width_request: 200,
+                    set_height_request: 200,
+                }
             }
         }
+
+        let widgets = Widgets { picture };
+
+        (my_box, widgets)
     }
 
-    fn init_model(path: Self::Init, _index: &DynamicIndex, _sender: FactorySender<Self>) -> Self {
-        Self { path }
+    fn bind(&mut self, widgets: &mut Self::Widgets, root: &mut Self::Root) {
+        widgets.picture.set_filename(Some(&self.path));
     }
 
-    fn update(&mut self, _msg: Self::Input, _sender: FactorySender<Self>) {}
+    //fn unbind(&mut self, widgets: &mut Self::Widgets, root: &mut Self::Root) {
+    //    widgets.picture.set_filename(None::<&path::Path>);
+    //}
 }
 
 pub struct AllPhotos {
     //    controller: photos_core::Controller,
-    pictures: FactoryVecDeque<PicturePreview>,
+    pictures_grid_view: TypedGridView<PicturePreview, gtk::SingleSelection>,
 }
 
 #[relm4::component(pub)]
@@ -65,14 +76,14 @@ impl SimpleComponent for AllPhotos {
             set_margin_all: 5,
 
             gtk::ScrolledWindow {
-                set_propagate_natural_height: true,
-                set_has_frame: true,
+                //set_propagate_natural_height: true,
+                //set_has_frame: true,
+                set_vexpand: true,
 
                 #[local_ref]
-                pictures_box -> gtk::FlowBox {
-                    set_max_children_per_line: 3,
-                    set_min_children_per_line: 3,
-                    set_homogeneous: false,
+                pictures_box -> gtk::GridView {
+                    set_orientation: gtk::Orientation::Vertical,
+                    set_max_columns: 3,
                 },
             },
         }
@@ -81,7 +92,7 @@ impl SimpleComponent for AllPhotos {
     fn init(
         _init: Self::Init,
         root: Self::Root,
-        sender: ComponentSender<Self>,
+        _sender: ComponentSender<Self>,
     ) -> ComponentParts<Self> {
         let repo = {
             let db_path = glib::user_data_dir().join("photo-romantic");
@@ -100,26 +111,25 @@ impl SimpleComponent for AllPhotos {
         // Time consuming!
         let _ = controller.scan();
 
-        let all_pictures = controller.all().unwrap();
-
-        let mut pictures = FactoryVecDeque::builder()
-            .launch(gtk::FlowBox::default())
-            .forward(sender.input_sender(), |_output| {});
-
-        for p in all_pictures.iter().take(10) {
+        let all_pictures = controller.all().unwrap().into_iter().map(|p| {
             let path = path::PathBuf::from("/var/home/david/Pictures").join(&p.relative_path);
-            pictures.guard().push_back(path);
-        }
+            PicturePreview { path }
+        });
 
-        let model = AllPhotos { pictures };
+        let mut grid_view_wrapper: TypedGridView<PicturePreview, gtk::SingleSelection> =
+            TypedGridView::new();
 
-        let pictures_box = model.pictures.widget();
-        pictures_box.set_max_children_per_line(3);
+        grid_view_wrapper.extend_from_iter(all_pictures.into_iter());
+
+        let model = AllPhotos {
+            pictures_grid_view: grid_view_wrapper,
+        };
+
+        let pictures_box = &model.pictures_grid_view.view;
 
         let widgets = view_output!();
-
         ComponentParts { model, widgets }
     }
 
-    fn update(&mut self, msg: Self::Input, _sender: ComponentSender<Self>) {}
+    fn update(&mut self, _msg: Self::Input, _sender: ComponentSender<Self>) {}
 }
