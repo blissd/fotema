e12a93a51cde5a5f552d93e86f265aaa52aaad00
David Bliss <vivid.map6767@davidbliss.me>
2024-04-01 10:28:27 +0100

    Initial scroll fix

--- src/app/components/year_photos.rs
+++ src/app/components/year_photos.rs
@@ -152,7 +152,9 @@ impl SimpleComponent for YearPhotos {
         };
 
         let pictures_box = &model.pictures_grid_view.view;
-        pictures_box.scroll_to(model.pictures_grid_view.len(), gtk::ListScrollFlags::SELECT, None);
+        if !model.pictures_grid_view.is_empty(){
+            pictures_box.scroll_to(model.pictures_grid_view.len() - 1, gtk::ListScrollFlags::SELECT, None);
+        }
 
         let widgets = view_output!();
         ComponentParts { model, widgets }
