972567051873935a949f66c353c5c1ee9d4edf82
David Bliss <vivid.map6767@davidbliss.me>
2024-04-10 04:51:16 +0100

    Use glcin for fallback thumbnail generation

--- core/src/preview.rs
+++ core/src/preview.rs
@@ -5,13 +5,12 @@
 use crate::repo;
 use crate::Error::*;
 use crate::Result;
+use gdk4::prelude::TextureExt;
+use glycin;
 use image::io::Reader as ImageReader;
 use image::DynamicImage;
 use std::path;
-use glycin;
-use gdk4::prelude::TextureExt;
 use tempfile::NamedTempFile;
-
 
 const EDGE: u32 = 200;
 
@@ -30,7 +29,7 @@ impl Previewer {
 
     /// Computes a preview square for an image that has been inserted
     /// into the Repository. Preview image will be written to file system.
-    pub async fn set_preview(&self, pic:  repo::Picture) -> Result<repo::Picture> {
+    pub async fn set_preview(&self, pic: repo::Picture) -> Result<repo::Picture> {
         if pic.square_preview_path.as_ref().is_some_and(|p| p.exists()) {
             return Ok(pic);
         }
@@ -91,19 +90,24 @@ impl Previewer {
     /// Copy an image to a PNG file using Glycin, and then use image-rs to compute the thumbnail.
     /// This is the fallback if image-rs can't decode the original image (such as HEIC images).
     async fn fallback_thumbnail(&self, path: &path::Path) -> Result<DynamicImage> {
-
         let file = gio::File::for_path(path);
 
-        let image = glycin::Loader::new(file).load().await
+        let image = glycin::Loader::new(file)
+            .load()
+            .await
             .map_err(|e| PreviewError(format!("Glycin load image: {}", e)))?;
 
-        let frame = image.next_frame().await
+        let frame = image
+            .next_frame()
+            .await
             .map_err(|e| PreviewError(format!("Glycin image frame: {}", e)))?;
 
-        let png_file = NamedTempFile::new()
-            .map_err(|e| PreviewError(format!("Temp file: {}", e)))?;
+        let png_file =
+            NamedTempFile::new().map_err(|e| PreviewError(format!("Temp file: {}", e)))?;
 
-        frame.texture.save_to_png(png_file.path())
+        frame
+            .texture
+            .save_to_png(png_file.path())
             .map_err(|e| PreviewError(format!("image save: {}", e)))?;
 
         self.standard_thumbnail(png_file.path())
