97707daf8388d8cad679bf971c05c76b5a51cbc7
David Bliss <vivid.map6767@davidbliss.me>
2024-06-17 16:34:15 +0100

    Use None filter when viewing individual item from map

--- src/app.rs
+++ src/app.rs
@@ -513,7 +513,7 @@ impl SimpleComponent for App {
         let places_page = PlacesAlbum::builder()
             .launch((state.clone(), active_view.clone()))
             .forward(sender.input_sender(), |msg| match msg {
-                PlacesAlbumOutput::View(visual_id) => AppMsg::View(visual_id, AlbumFilter::All),
+                PlacesAlbumOutput::View(visual_id) => AppMsg::View(visual_id, AlbumFilter::None),
                 PlacesAlbumOutput::GeographicArea(cell_index) => AppMsg::ViewGeographicArea(cell_index),
             });
 
