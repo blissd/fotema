ea98d33e3e85b461c71d0d18e62d1be3f3afbada
David <vivid.map6767@davidbliss.me>
2024-10-18 06:06:24 +0100

    Reapply "Merge pull request #231 from blissd/gnome_47"
    This reverts commit fb31b3e033485c8f13d06fe78c5797391e8c22dc.

--- src/app.rs
+++ src/app.rs
@@ -187,7 +187,7 @@ pub(super) struct App {
     header_bar: adw::HeaderBar,
 
     // Activity indicator. Only shown when progress bar is hidden.
-    spinner: gtk::Spinner,
+    spinner: adw::Spinner,
 
     bootstrap_progress: Controller<ProgressPanel>,
 
@@ -364,7 +364,7 @@ impl SimpleComponent for App {
                                     },
 
                                     #[local_ref]
-                                    pack_end = &spinner -> gtk::Spinner,
+                                    pack_end = &spinner -> adw::Spinner,
                                 },
 
                                 // NOTE I would like this to be an adw::ViewStack
@@ -696,7 +696,7 @@ impl SimpleComponent for App {
 
         let header_bar = adw::HeaderBar::new();
 
-        let spinner = gtk::Spinner::builder().visible(false).build();
+        let spinner = adw::Spinner::builder().visible(false).build();
 
         let banner = adw::Banner::builder()
             .button_label(fl!("banner-button-stop", "label"))
@@ -768,9 +768,6 @@ impl SimpleComponent for App {
         // Get startup window size and propagate so all components have correct narrow/wide layout.
         sender.input(AppMsg::Activate(widgets.main_window.default_width()));
 
-        model.spinner.set_visible(true);
-        model.spinner.start();
-
         let settings = settings_state.read();
         let is_onboarding_complete = settings.is_onboarding_complete && settings.pictures_base_dir.exists();
         if is_onboarding_complete {
@@ -806,7 +803,11 @@ impl SimpleComponent for App {
             AppMsg::ToggleSidebar => {
                 let show = self.main_navigation.shows_sidebar();
                 self.main_navigation.set_show_sidebar(!show);
-                self.spinner.set_visible(show);
+
+                // FIXME refactor model to not store a bunch of widgets and instead
+                // have some enums to represent current app states.
+                let task_running = self.banner.is_revealed();
+                self.spinner.set_visible(show && task_running);
             },
             AppMsg::SwitchView => {
                 let child = self.main_stack.visible_child();
@@ -888,7 +889,6 @@ impl SimpleComponent for App {
                 self.people_page.emit(PeopleAlbumInput::Refresh);
             },
             AppMsg::TaskStarted(task_name) => {
-                self.spinner.start();
                 self.spinner.set_visible(!self.main_navigation.shows_sidebar());
                 self.banner.set_revealed(true);
                 self.banner.set_button_label(Some(&fl!("banner-button-stop", "label")));
@@ -937,7 +937,7 @@ impl SimpleComponent for App {
             },
             AppMsg::BootstrapCompleted => {
                 info!("Bootstrap completed.");
-                self.spinner.stop();
+                self.spinner.set_visible(false);
                 self.banner.set_revealed(false);
             },
             AppMsg::TranscodeAll => {
