d9db5a98baaa1f0c52946f2c0c295dd5409facbf
David Bliss <vivid.map6767@davidbliss.me>
2025-05-10 09:55:44 +0100

    Generate multiple thumbnail sizes

--- src/app/components/albums/album.rs
+++ src/app/components/albums/album.rs
@@ -174,8 +174,14 @@ impl RelmGridItem for PhotoGridItem {
             widgets.is_bound = true;
         }
 
+        let thumbnail_size = if self.edge_length.value() == NARROW_EDGE_LENGTH {
+            ThumbnailSize::Normal
+        } else {
+            ThumbnailSize::Large
+        };
+
         let thumbnail_path = self.thumbnailer
-            .nearest_thumbnail(&self.visual.thumbnail_hash(), ThumbnailSize::Large);
+            .nearest_thumbnail(&self.visual.thumbnail_hash(), thumbnail_size);
 
         if thumbnail_path.is_some() {
             widgets
