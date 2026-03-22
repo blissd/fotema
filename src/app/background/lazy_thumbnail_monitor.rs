74789fc7d4322fcd1ece546cac7891123324e169
David Bliss <vivid.map6767@davidbliss.me>
2026-03-22 14:15:30 +0000

    feat: Add lazy thumbnail monitor

---
+++ src/app/background/lazy_thumbnail_monitor.rs
@@ -0,0 +1,35 @@
+// SPDX-FileCopyrightText: © 2026 David Bliss
+//
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+use fotema_core::VisualId;
+use relm4::{Reducer, Reducible};
+use std::sync::Arc;
+
+type LazyThumbnailState = Arc<Reducer<LazyThumbnailMonitor>>;
+
+#[derive(Debug)]
+pub enum LazyThumbnailMonitorInput {
+    Completed(VisualId),
+}
+
+/// Exposes completed lazy thumbnail loads to subscribers.
+pub struct LazyThumbnailMonitor {
+    // A thumbnail has been generated for photo or video.
+    pub completed: Option<VisualId>,
+}
+
+impl Reducible for LazyThumbnailMonitor {
+    type Input = LazyThumbnailMonitorInput;
+
+    fn init() -> Self {
+        Self { completed: None }
+    }
+
+    fn reduce(&mut self, input: Self::Input) -> bool {
+        match input {
+            LazyThumbnailMonitorInput::Completed(visual_id) => self.completed = Some(visual_id),
+        }
+        true // subscribers only notified if 'true' is returned
+    }
+}
