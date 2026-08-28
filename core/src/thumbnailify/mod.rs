562ba9202db745f3e71927295acf8215e82d3cf2
David Bliss <vivid.map6767@davidbliss.me>
2026-08-28 20:40:31 +0100

    feat: Move Thumbnailer

--- core/src/thumbnailify/mod.rs
+++ core/src/thumbnailify/mod.rs
@@ -2,9 +2,6 @@
 //
 // SPDX-License-Identifier: GPL-3.0-or-later
 
-use image::DynamicImage;
-use std::path::{Path, PathBuf};
-
 pub mod error;
 pub mod file;
 pub mod hash;
@@ -22,90 +19,4 @@ pub use hash::compute_hash;
 pub use hash::compute_hash_for_path;
 pub use quality::ThumbnailQuality;
 pub use sizes::ThumbnailSize;
-
-use crate::FlatpakPathBuf;
-
-#[derive(Clone, Debug)]
-pub struct Thumbnailer {
-    thumbnails_path: PathBuf,
-}
-
-impl Thumbnailer {
-    pub fn build(thumbnails_path: &Path) -> Thumbnailer {
-        Thumbnailer {
-            thumbnails_path: thumbnails_path.into(),
-        }
-    }
-
-    pub fn is_failed(&self, host_path: &Path) -> bool {
-        file::is_failed(&self.thumbnails_path, host_path)
-    }
-
-    pub fn is_thumbnail_up_to_date(&self, host_path: &Path) -> bool {
-        file::is_thumbnail_up_to_date(&self.thumbnails_path, host_path)
-    }
-
-    pub fn get_thumbnail_hash_output(&self, hash: &str, size: ThumbnailSize) -> PathBuf {
-        get_thumbnail_hash_output(&self.thumbnails_path, hash, size)
-    }
-
-    pub fn get_thumbnail_path(&self, host_path: &Path, size: ThumbnailSize) -> PathBuf {
-        get_thumbnail_path(&self.thumbnails_path, host_path, size)
-    }
-
-    //pub fn nearest_thumbnail_by_dimension(&self, hash: &str, dimension: u32) -> Option<PathBuf> {
-    //}
-
-    /**
-     * Compute thumbnail path, or sensible fallback if preferred size does not exist.
-     * If no thumbnails exist, then return preferred path pointing to absent file.
-     */
-    pub fn nearest_thumbnail(&self, hash: &str, size: ThumbnailSize) -> Option<PathBuf> {
-        let preferred = file::get_thumbnail_hash_output(&self.thumbnails_path, hash, size);
-
-        if preferred.exists() {
-            Some(preferred)
-        } else {
-            let xxlarge = file::get_thumbnail_hash_output(
-                &self.thumbnails_path,
-                hash,
-                ThumbnailSize::XXLarge,
-            );
-            let xlarge =
-                file::get_thumbnail_hash_output(&self.thumbnails_path, hash, ThumbnailSize::XLarge);
-            let large =
-                file::get_thumbnail_hash_output(&self.thumbnails_path, hash, ThumbnailSize::Large);
-            let normal =
-                file::get_thumbnail_hash_output(&self.thumbnails_path, hash, ThumbnailSize::Normal);
-            let small =
-                file::get_thumbnail_hash_output(&self.thumbnails_path, hash, ThumbnailSize::Small);
-
-            let paths = match size {
-                // TODO figure out if some fallback sizes should be excluded?
-                // Do I want a request for a small thumbnail to return an XXLarge?
-                ThumbnailSize::Small => [small, normal, large, xlarge, xxlarge],
-                ThumbnailSize::Normal => [normal, large, xlarge, xxlarge, small],
-                ThumbnailSize::Large => [large, xlarge, xxlarge, normal, small],
-                ThumbnailSize::XLarge => [xlarge, xxlarge, large, normal, small],
-                ThumbnailSize::XXLarge => [xxlarge, xlarge, large, normal, small],
-            };
-
-            paths.iter().find(|path| path.exists()).cloned()
-        }
-    }
-
-    pub fn generate_thumbnail(
-        &self,
-        path: &FlatpakPathBuf,
-        size: ThumbnailSize,
-        quality: ThumbnailQuality,
-        src_image: DynamicImage,
-    ) -> Result<(), ThumbnailError> {
-        thumbnailer::generate_thumbnail(&self.thumbnails_path, path, size, quality, src_image)?;
-        Ok(())
-    }
-
-    pub fn write_failed_thumbnail(&self, path: &FlatpakPathBuf) -> Result<(), ThumbnailError> {
-        file::write_failed_thumbnail(&self.thumbnails_path, path)
-    }
-}
+pub use thumbnailer::Thumbnailer;
--- core/src/thumbnailify/thumbnailer.rs
+++ core/src/thumbnailify/thumbnailer.rs
@@ -3,15 +3,17 @@
 //
 // SPDX-License-Identifier: GPL-3.0-or-later
 
-use std::io::{BufReader, BufWriter};
-use std::{fs, fs::File, io, path::Path, time::UNIX_EPOCH};
+use std::io::BufWriter;
+use std::{fs, io, path::Path, path::PathBuf, time::UNIX_EPOCH};
 
-use tracing::{debug, info};
+use tracing::info;
 
 use crate::FlatpakPathBuf;
-use crate::thumbnailify::{
+
+use super::{
     ThumbnailQuality,
     error::ThumbnailError,
+    file,
     file::{
         get_failed_thumbnail_output, get_file_uri, get_thumbnail_hash_output,
         is_thumbnail_up_to_date,
@@ -21,7 +23,6 @@ use crate::thumbnailify::{
 };
 
 use image::{DynamicImage, ImageBuffer, Rgba};
-use png::Decoder;
 
 use fast_image_resize as fr;
 use fr::images::Image;
@@ -30,6 +31,88 @@ use fr::{ResizeOptions, Resizer};
 use png::Encoder as ExtendedPngEncoder;
 
 use tempfile;
+
+#[derive(Clone, Debug)]
+pub struct Thumbnailer {
+    thumbnails_path: PathBuf,
+}
+
+impl Thumbnailer {
+    pub fn build(thumbnails_path: &Path) -> Thumbnailer {
+        Thumbnailer {
+            thumbnails_path: thumbnails_path.into(),
+        }
+    }
+
+    pub fn is_failed(&self, host_path: &Path) -> bool {
+        file::is_failed(&self.thumbnails_path, host_path)
+    }
+
+    pub fn is_thumbnail_up_to_date(&self, host_path: &Path) -> bool {
+        file::is_thumbnail_up_to_date(&self.thumbnails_path, host_path)
+    }
+
+    pub fn get_thumbnail_hash_output(&self, hash: &str, size: ThumbnailSize) -> PathBuf {
+        file::get_thumbnail_hash_output(&self.thumbnails_path, hash, size)
+    }
+
+    pub fn get_thumbnail_path(&self, host_path: &Path, size: ThumbnailSize) -> PathBuf {
+        file::get_thumbnail_path(&self.thumbnails_path, host_path, size)
+    }
+
+    /**
+     * Compute thumbnail path, or sensible fallback if preferred size does not exist.
+     * If no thumbnails exist, then return preferred path pointing to absent file.
+     */
+    pub fn nearest_thumbnail(&self, hash: &str, size: ThumbnailSize) -> Option<PathBuf> {
+        let preferred = file::get_thumbnail_hash_output(&self.thumbnails_path, hash, size);
+
+        if preferred.exists() {
+            Some(preferred)
+        } else {
+            let xxlarge = file::get_thumbnail_hash_output(
+                &self.thumbnails_path,
+                hash,
+                ThumbnailSize::XXLarge,
+            );
+            let xlarge =
+                file::get_thumbnail_hash_output(&self.thumbnails_path, hash, ThumbnailSize::XLarge);
+            let large =
+                file::get_thumbnail_hash_output(&self.thumbnails_path, hash, ThumbnailSize::Large);
+            let normal =
+                file::get_thumbnail_hash_output(&self.thumbnails_path, hash, ThumbnailSize::Normal);
+            let small =
+                file::get_thumbnail_hash_output(&self.thumbnails_path, hash, ThumbnailSize::Small);
+
+            let paths = match size {
+                // TODO figure out if some fallback sizes should be excluded?
+                // Do I want a request for a small thumbnail to return an XXLarge?
+                ThumbnailSize::Small => [small, normal, large, xlarge, xxlarge],
+                ThumbnailSize::Normal => [normal, large, xlarge, xxlarge, small],
+                ThumbnailSize::Large => [large, xlarge, xxlarge, normal, small],
+                ThumbnailSize::XLarge => [xlarge, xxlarge, large, normal, small],
+                ThumbnailSize::XXLarge => [xxlarge, xlarge, large, normal, small],
+            };
+
+            paths.iter().find(|path| path.exists()).cloned()
+        }
+    }
+
+    pub fn generate_thumbnail(
+        &self,
+        path: &FlatpakPathBuf,
+        size: ThumbnailSize,
+        quality: ThumbnailQuality,
+        src_image: DynamicImage,
+    ) -> Result<(), ThumbnailError> {
+        generate_thumbnail(&self.thumbnails_path, path, size, quality, src_image)?;
+        Ok(())
+    }
+
+    pub fn write_failed_thumbnail(&self, path: &FlatpakPathBuf) -> Result<(), ThumbnailError> {
+        file::write_failed_thumbnail(&self.thumbnails_path, path)
+    }
+}
 
 /// Generate a thumbnail for a file that exists outside of the Flatpak sandbox.
 /// NOTE: the sandbox_path/host_path could point to a picture or a video.
